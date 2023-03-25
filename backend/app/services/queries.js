65f807a19b680282bcf153db1e4eaa21d1ec3bc5
Bohdan Drozd <drozd.bogdan35@gmail.com>
2023-03-24 22:22:25 -0400

    "DB_queries_update"

--- backend/app/services/queries.js
+++ backend/app/services/queries.js
@@ -53,4 +53,53 @@ async function getRecipeCategory(id) {
   // Fill
 }
 
+//Create query to INSERT  new recipes into the Recipe Table - DAN
+export async function insertNewRecipe() 
+{
+  const [rows] = await pool.query(`INSERT INTO foodhelperDB.recipes (name,recipe_categoryid,recipe_photo,recipe_description) 
+  VALUES(name.value,categoryField.value,photoUrl.value,recipeDesk.value)`);//Needs to be updated with the corresponding values fields from the user outputs in the form
+  console.log(rows);
+  return rows;
+}
+
+//Update query to SET new info into the Recipe Table by id - DAN
+export async function updateExistingRecipeById()
+{
+  const [rows] = await pool.query(`UPDATE foodhelperDB.recipes 
+  SET name="Recipe name", recipe_photo="UserPhotourl", recipe_description="Updated Desc." 
+  WHERE id=id.value`,[id]);//Needs to be updated with the corresponding values fields from the user outputs in the form and the particular id
+  console.log(rows);
+  return rows;
+}
+
+//Delete query from the Recipe Table by id - DAN
+export async function deleteExistingRecipeById()
+{
+  const [rows] = await pool.query(`DELETE FROM recipes 
+  WHERE id=id.value`,[id]);//Needs to be updated with the corresponding values field from the user outputs in the form and the particular id
+  console.log(rows);
+  return rows;
+}
+
+//SELECT recipes in the Recipe Table SPECIFIC to the FILTER(recipe name) input by user - DAN
+export async function findExistingRecipeByName()
+{
+  const [rows] = await pool.query(`SELECT name,likes,dislike,recipe_photo,recipe_description,category_name FROM foodhelperDB.recipes
+  INNER JOIN recipe_categories ON recipe_categories.id = recipes.recipe_categoryid
+  WHERE name = "recipe_name.value"`);//Needs to be updated with the corresponding value field from the user outputs in the form and the particular NAME
+  console.log(rows);
+  return rows;
+}
+
+//SELECT ingredients in the Ingredients Table SPECIFIC to the FILTER input by user - DAN
+export async function findExistingIngredientByName()
+{
+  const [rows] = await pool.query(`SELECT name,calories,price,ingredient_photo FROM foodhelperDB.ingredients
+  INNER JOIN ingredient_category ON ingredient_category.id = ingredients.ingredient_category
+  WHERE name = "user_ingredient.value"`);//Needs to be updated with the corresponding value field from the user outputs in the form and the particular Ingredient
+  console.log(rows);
+  return rows;
+}
+
+
 module.exports = { getIngredient, getAllIngredients };
