d4475984a58ea457958cb7eaeaacb5f8449cb9ff
Alejandro A <ale@doges.dev>
2023-04-17 06:50:10 -0400

    BrowseRecipes: added live filter that searches recipe names

--- frontend/app/components/browseRecipes/BrowseRecipes.js
+++ frontend/app/components/browseRecipes/BrowseRecipes.js
@@ -6,6 +6,7 @@ import ReactMarkdown from 'react-markdown'
 
 function BrowseRecipes() {
   const [recipes, setRecipes] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     async function fetchRecipes() {
@@ -27,26 +28,60 @@ function BrowseRecipes() {
     return `https://${bucketName}.s3.amazonaws.com/${key}`;
   }
 
+  // filter recipes based on search query
+  const filteredRecipes = recipes.filter((recipe) =>
+    recipe.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   
   return (
     <div className="container mt-3">
       <h2>Browse Recipes</h2>
       <div className="row mt-3">
-        {recipes.map(recipe => (
+        <div className="col-xl-12 col-lg-10 col-md-12 mb-3">
+          <div className="input-group">
+            <span className="input-group-text">Filter by Recipe Name:</span>
+            <input
+              type="text"
+              className="form-control "
+              placeholder="Type a dish name..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+        </div>
+        </div>
+        <div className="row mt-3">
+        {filteredRecipes.map((recipe) => (
           <div key={recipe.id} className="col-md-4 mb-3">
             <div className="card h-100">
-              <img src={getS3PublicUrl(recipe.recipe_photo)} className="card-img-top" alt="Recipe" />
+              <img
+                src={getS3PublicUrl(recipe.recipe_photo)}
+                className="card-img-top"
+                alt="Recipe"
+              />
               <div className="card-body">
                 <h5 className="card-title">{recipe.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{recipe.category_name}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  {recipe.category_name}
+                </h6>
                 <div className="card-text">
-                    <ReactMarkdown>{recipe.recipe_description.slice(0,80)}</ReactMarkdown>...
-                    </div>
+                  <ReactMarkdown>
+                    {recipe.recipe_description.slice(0, 80)}
+                  </ReactMarkdown>
+                  ...
+                </div>
               </div>
               <div className="card-footer">
                 <small className="text-muted">
                   <span className="me-2">{recipe.likes} likes</span>
-                  <span>{recipe.ingredients.reduce((acc, i) => acc + i.calories, 0)} kcal</span>
+                  <span>
+                    {recipe.ingredients.reduce(
+                      (acc, i) => acc + i.calories,
+                      0
+                    )}{" "}
+                    kcal
+                  </span>
                 </small>
               </div>
             </div>
