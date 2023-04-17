7f914a1e3f8406873beb3c3ddf9479fd36bbb6c1
Alejandro A <ale@doges.dev>
2023-04-17 13:49:59 -0400

    View recipe complete

--- frontend/app/components/viewRecipe/viewRecipe.js
+++ frontend/app/components/viewRecipe/viewRecipe.js
@@ -1,31 +1,65 @@
-import React from "react";
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import ReactMarkdown from "react-markdown";
+
+import { useParams } from "react-router-dom";
 
 function ViewRecipe() {
-    const {id} = useParams();
+  const { id } = useParams(); // get the recipe ID from the URL
+
+  const [recipe, setRecipe] = useState(null);
+
+  useEffect(() => {
+    async function fetchRecipe() {
+      try {
+        const response = await Axios.get(`/api/getRecipeById/${id}`);
+        setRecipe(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchRecipe();
+  }, [id]);
+
+  if (!recipe) {
+    return <div>Loading...</div>;
+  }
+
+  // function to parse s3 urls
+  function getS3PublicUrl(s3Url) {
+    if (s3Url.split(":")[0] === "https") {
+      return s3Url;
+    }
+    //check 2 first characters
+    const urlParts = s3Url.split("/");
+    const bucketName = urlParts[2];
+    const key = urlParts.slice(3).join("/");
+    return `https://${bucketName}.s3.amazonaws.com/${key}`;
+  }
+
   return (
-    <div>
-      <h1>View Recipe</h1>
-      <p>Recipe Name: Pasta Carbonara</p>
-      <p>Ingredients:</p>
+    <div className="container mt-3">
+      <h2>{recipe.name}</h2>
+      <img
+        src={getS3PublicUrl(recipe.recipe_photo)}
+        className="img-fluid mt-3 mb-3"
+        alt="Recipe"
+      />
+      <h5>Category: {recipe.category_name}</h5>
+      <h5>Ingredients:</h5>
       <ul>
-        <li>Spaghetti</li>
-        <li>Egg yolks</li>
-        <li>Bacon</li>
-        <li>Heavy cream</li>
-        <li>Parmesan cheese</li>
-        <li>Black pepper</li>
+        {recipe.ingredients.map((ingredient) => (
+          <li key={ingredient.id}>
+            {ingredient.quantity} {ingredient.unit} {ingredient.name} (
+            {ingredient.calories} kcal)
+          </li>
+        ))}
       </ul>
-      <p>Instructions:</p>
-      <ol>
-        <li>Cook spaghetti in salted boiling water until al dente.</li>
-        <li>Cut bacon into small pieces and fry until crispy.</li>
-        <li>In a bowl, whisk egg yolks, heavy cream, Parmesan cheese, and black pepper until combined.</li>
-        <li>Drain the spaghetti and add it to the frying pan with the bacon. Toss to combine.</li>
-        <li>Remove the pan from the heat and add the egg mixture. Toss to combine.</li>
-        <li>Serve hot and enjoy!</li>
-      </ol>
-      <h2>{id}</h2>
+      <h5>Instructions:</h5>
+      <ReactMarkdown>{recipe.recipe_description}</ReactMarkdown>
+      <div className="mt-3">
+        <small className="text-muted">{recipe.likes} likes</small>
+      </div>
     </div>
   );
 }
