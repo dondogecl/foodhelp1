c5b1b5bdc72d7b379072e115f86c7ef6fa0803ad
Alejandro A <ale@doges.dev>
2023-04-17 06:03:23 -0400

    before installin markdown

--- frontend/app/components/browseRecipes/BrowseRecipes.js
+++ frontend/app/components/browseRecipes/BrowseRecipes.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
+import {Remarkable} from 'remarkable';
+
 
 function BrowseRecipes() {
   const [recipes, setRecipes] = useState([]);
@@ -16,6 +18,16 @@ function BrowseRecipes() {
     fetchRecipes();
   }, []);
 
+  // function to parse s3 urls
+  function getS3PublicUrl(s3Url) {
+    const urlParts = s3Url.split('/');
+    const bucketName = urlParts[2];
+    const key = urlParts.slice(3).join('/');
+    return `https://${bucketName}.s3.amazonaws.com/${key}`;
+  }
+
+
+  
   return (
     <div className="container mt-3">
       <h2>Browse Recipes</h2>
@@ -23,11 +35,12 @@ function BrowseRecipes() {
         {recipes.map(recipe => (
           <div key={recipe.id} className="col-md-4 mb-3">
             <div className="card h-100">
-              <img src={recipe.recipe_photo} className="card-img-top" alt="Recipe" />
+              <img src={getS3PublicUrl(recipe.recipe_photo)} className="card-img-top" alt="Recipe" />
               <div className="card-body">
                 <h5 className="card-title">{recipe.name}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{recipe.category_name}</h6>
-                <p className="card-text">{recipe.recipe_description.slice(0,60)}</p>
+                <p className="card-text">
+                    </p>
               </div>
               <div className="card-footer">
                 <small className="text-muted">
